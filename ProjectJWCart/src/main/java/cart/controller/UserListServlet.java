8f207c2e1a308cd1f0c28ba63b9f5d573d18f8b2
KrisYangPO <kristoff0109@gmail.com>
2025-04-28 16:31:19 +0800

    List

--- ProjectJWCart/src/main/java/cart/controller/UserListServlet.java
+++ ProjectJWCart/src/main/java/cart/controller/UserListServlet.java
@@ -1,7 +1,11 @@
 package cart.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import cart.model.dto.UserDTO;
+import cart.service.impl.UserListService;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,10 +15,16 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/user/list")
 public class UserListServlet extends HttpServlet {
 
+	private  UserListService service = new UserListService();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/WEB-INF/view/cart/user_list.jsp").forward(req, resp);
 		
+		// 顯示所有內容。
+		List<UserDTO> users = service.findAllUsers();
+		req.setAttribute("users", users);
+		//導向頁面
+		req.getRequestDispatcher("/WEB-INF/view/cart/user_list.jsp").forward(req, resp);
 	}
 
 	
