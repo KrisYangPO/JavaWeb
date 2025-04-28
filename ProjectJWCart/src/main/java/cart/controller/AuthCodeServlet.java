8dee09760d341a3ec9c28e32e37da1f066c51429
KrisYangPO <kristoff0109@gmail.com>
2025-04-28 14:21:20 +0800

    authcode

--- ProjectJWCart/src/main/java/cart/controller/AuthCodeServlet.java
+++ ProjectJWCart/src/main/java/cart/controller/AuthCodeServlet.java
@@ -18,11 +18,24 @@ import jakarta.servlet.http.HttpServletResponse;
 // 產生認證碼的 Servlet
 @WebServlet("/user/authcode")
 public class AuthCodeServlet extends HttpServlet {
+	
+	// 自訂認證碼 0~9 a-z A-Z
+	private String generateAuthCode() {
+		String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		StringBuffer authcode = new StringBuffer();
+		Random random = new Random();
+		for(int i=0;i<4;i++) {
+			int index = random.nextInt(chars.length()); // 隨機取位置
+			authcode.append(chars.charAt(index)); // 取得該位置的資料
+		}
+		return authcode.toString();
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Random random = new Random();
-		String authcode = String.format("%04d", random.nextInt(10000)); // 0000~9999 的隨機數
+//		String authcode = String.format("%04d", random.nextInt(10000)); // 0000~9999 的隨機數
+		String authcode = generateAuthCode();
 		ImageIO.write(getAuthCodeImage(authcode), "JPEG", resp.getOutputStream());
 	}
 	
@@ -42,6 +55,19 @@ public class AuthCodeServlet extends HttpServlet {
 		g.setFont(new Font("Arial", Font.BOLD, 22));
 		// 繪文字 (18~22) 表示繪文字的左上角起點。
 		g.drawString(authcode, 18, 22); 
+		
+		// 加入干擾線：
+		g.setColor(Color.RED);
+		Random random = new Random();
+		for(int i=0; i<15; i++) {
+			// 座標點
+			int x1 = random.nextInt(80);
+			int y1 = random.nextInt(30);
+			int x2 = random.nextInt(80);
+			int y2 = random.nextInt(30);
+			// 繪製直線
+			g.drawLine(x1, y1, x2, y2);
+		}
 	
 		return img;
 	}
