582ca44f8a7ea8ed09627fbe1f9cbd9bb70587d5
KrisYangPO <kristoff0109@gmail.com>
2025-05-07 15:18:01 +0800

    sum

--- Spring/springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ Spring/springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -82,19 +86,94 @@ public class ApiController {
 	 * 
 	 * */
 	
-	// 設定 produces = "application/json;charset = utf-8" 就會將物件轉成 JSON 格式。
-	@GetMapping(value = "/bmi", produces = "application/json;charset = utf-8")
-	public ApiResponse calBMI(
-			@RequestParam(value = "h", required = false) Double hei, 
-			@RequestParam(value = "w", required = false) Double wei
-			) {
+//	// 設定 produces = "application/json;charset = utf-8" 就會將物件轉成 JSON 格式。
+//	@GetMapping(value = "/bmi", produces = "application/json;charset = utf-8")
+//	public ApiResponse calBMI(
+//			@RequestParam(value = "h", required = false) Double hei, 
+//			@RequestParam(value = "w", required = false) Double wei
+//			) {
+//		
+//		if(hei == null || wei == null) {
+//			return ApiResponse.error(400, "Please provide weight and height.");
+//		}
+//		
+//		// 計算 bmi
+//		Double bmi = wei/ ((hei/100.0)*(hei/100.0));
+//		return ApiResponse.success("BMI testing success.", new BMI(hei, wei, bmi));
+//	}
+	
+	
+	// 不親自設定狀態碼：
+	// 將 ApiResponse 有關狀態碼和 status 的屬性與參數移除，
+	@GetMapping(value = "/bmi", produces = "application/json;charset=utf-8")
+	public ResponseEntity<ApiResponse<BMI>> calcBmi(
+			@RequestParam(required = false) Double h, 
+			@RequestParam(required = false) Double w) {
 		
-		if(hei == null || wei == null) {
-			return ApiResponse.error(400, "Please provide weight and height.");
+		if(h == null || w == null) {
+			// badRequest().body() 回應發生錯誤時，要在 body() 執行什麼資訊
+			return ResponseEntity.badRequest().body(ApiResponse.error("請提供身高(h)或體重(w)"));
 		}
 		
-		// 計算 bmi
-		Double bmi = wei/ ((hei/100.0)*(hei/100.0));
-		return ApiResponse.success("BMI testing success.", new BMI(hei, wei, bmi));
+		double bmi = w / Math.pow(h/100, 2);
+		// 當程式沒問題，ok()當中執行什麼
+		return ResponseEntity.ok(ApiResponse.success("BMI 計算成功", new BMI(h, w, bmi)));
 	}
+	
+	
+	/* 5. 同名多筆資料
+	 * 路徑：/age?age=17&age=18&age=20
+	 * 網址：http://localhost:8080/api/age?age=17&age=18&age=20
+	 * 算出平均年齡
+	 */
+	@GetMapping(value = "/age", produces = "application/json;charset = utf-8")
+	public ResponseEntity<ApiResponse<Object>> getAverage(
+			// 多參數帶入 List 當中
+			@RequestParam("age") List<String> ages) {
+		
+		if(ages == null || ages.size() == 0) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("輸入一個 age"));
+		}
+		
+		// 計算平均，轉成 integer 在計算平均
+		double avg = ages.stream().mapToInt(Integer::parseInt).average().orElseGet(()-> 0); 
+		// 建立 data 資料，為 Map 資料型態，帶入 ApiResponse.success() 當中。
+		Object map = Map.of("年齡", ages, "平均年齡", String.format("%.1f", avg));
+		
+		return ResponseEntity.ok(ApiResponse.success("計算成功", map));
+	}
+	
+	
+	/* 6. Lab 練習: 得到多筆 score 資料
+	 * 路徑: "/exam?score=80&score=100&score=50&score=70&score=30"
+	 * 網址: http://localhost:8080/api/exam?score=80&score=100&score=50&score=70&score=30
+	 * 請自行設計一個方法，此方法可以
+	 * 印出: 最高分=?、最低分=?、平均=?、總分=?、及格分數列出=?、不及格分數列出=?
+	 */
+	@GetMapping(value = "/score", produces = "application/json;charset = utf-8")
+	public ResponseEntity<ApiResponse<Object>> scoreSummary(
+			// 多參數帶入 List 當中
+			@RequestParam("score") List<String> scores){
+		
+		if(scores == null || scores.size() == 0) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("輸入一個 score"));
+		}
+		
+		// 計算最高分
+		Integer max = scores.stream().mapToInt(Integer::parseInt).max().orElseGet(()-> 0); 
+		// 計算最低分
+		Integer min = scores.stream().mapToInt(Integer::parseInt).min().orElseGet(()-> 0); 
+		// 計算平均
+		Double avg = scores.stream().mapToInt(Integer::parseInt).average().orElseGet(()-> 0); 
+		// 總分
+		Integer sum = scores.stream().mapToInt(Integer::parseInt).sum().orElseGet(()-> 0); 
+		// 及格分數
+		List<Integer> pass = scores.stream().mapToInt(Integer::parseInt).filter(s -> s>60).toArray();
+		// 不及格分數
+		List<Integer> fail = scores.stream().mapToInt(Integer::parseInt).filter(s -> s<60).toArray();
+		
+		
+	}// end of ScoreSummary
+	
+	
 }
--- Spring/springboot-mvc/src/main/java/com/example/demo/response/ApiResponse.java
+++ Spring/springboot-mvc/src/main/java/com/example/demo/response/ApiResponse.java
@@ -1,26 +1,52 @@
+//package com.example.demo.response;
+//
+//import lombok.*;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+// 建立 Server 與 Client 在資料傳遞上的統一結構與標準
+//public class ApiResponse<T> {
+//	
+//	private Integer status; // 狀態：200, 400
+//	private String message; // 訊息：查詢成功，新增成功...
+//	T data;	 // 實際資料 (payLoad)
+//	
+//	// 成功回應
+//	// 方法使用泛形時，要在方法回傳值資料型態前加上<T>，告訴使用者這個方法使用了泛形，
+//	// 後面 ApiResponse<T> 才時泛形方法的資料型態。
+//	public static <T> ApiResponse<T> success(String message, T data){
+//		return new ApiResponse<T>(200, message, data);
+//	}
+//	
+//	// 失敗回應
+//	public static <T> ApiResponse<T> error(int status, String message){
+//		return new ApiResponse<T>(status, message, null);
+//	}
+//}
+
+
+// 利用 responseEntity 方式自動抓取 status
 package com.example.demo.response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import lombok.*;
-
+// 建立 Server 與 Client 在傳遞資料上的統一結構與標準(含錯誤)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-// 建立 Server 與 Client 在資料傳遞上的統一結構與標準
 public class ApiResponse<T> {
-	
-	private Integer status; // 狀態：200, 400
-	private String message; // 訊息：查詢成功，新增成功...
-	T data;	 // 實際資料 (payLoad)
+	private String message; // 訊息 例如: 查詢成功, 新增成功, 計算成功...
+	T data;                 // payload 實際資料
 	
 	// 成功回應
-	// 方法使用泛形時，要在方法回傳值資料型態前加上<T>，告訴使用者這個方法使用了泛形，
-	// 後面 ApiResponse<T> 才時泛形方法的資料型態。
-	public static <T> ApiResponse<T> success(String message, T data){
-		return new ApiResponse<T>(200, message, data);
+	public static <T> ApiResponse<T> success(String message, T data) {
+		return new ApiResponse<T>(message, data);
 	}
 	
 	// 失敗回應
-	public static <T> ApiResponse<T> error(int status, String message){
-		return new ApiResponse<T>(status, message, null);
+	public static <T> ApiResponse<T> error(String message) {
+		return new ApiResponse<T>(message, null);
 	}
 }
