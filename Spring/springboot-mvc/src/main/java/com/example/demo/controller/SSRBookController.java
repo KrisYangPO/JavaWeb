68cb88f33cd36cce186bbd91eb21915f70f484e8
KrisYangPO <kristoff0109@gmail.com>
2025-05-16 12:05:58 +0800

    bookcontroller

--- Spring/springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
+++ Spring/springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
@@ -2,12 +2,17 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.modelmapper.internal.bytebuddy.utility.privilege.GetMethodAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.exception.BookException;
 import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
 
@@ -18,10 +23,48 @@ public class SSRBookController {
 	private BookService bookService;
 	
 	// 查詢所有書籍
-	@GetMapping
+	@GetMapping // 跑 "/ssr/book"
 	public String findAllBooks(Model model) {
+		// 要在 Spring framework 將資料傳給 JSP 方式是由 Model 容器儲存 Attribute，
+		// 類似之前的 request.setAttributes() 的方式，request 由 Model 取代。
+		
 		List<Book> books = bookService.findAllBooks();
 		model.addAttribute("books", books);
-		return "book-list"; // 對應到 "/WEB-INF/view/book-list.jsp"
+		return "book-list"; 
+		// 對應到 "/WEB-INF/view/book-list.jsp"
+		// 簡化 req.getRequestDispatcher("jsp").forward(req, resp); 
+	}
+	
+	
+	// 新增書籍
+	@PostMapping("/add")
+	public String addBook(Book book, Model model) {
+		try {
+			bookService.addBook(book);
+		} catch (BookException e) {
+			model.addAttribute("message", "新增時發生錯誤" + e.getMessage());
+			return "error";
+		}
+		return "redirect:/ssr/book"; 
+		/* 重新執行 ssr/book
+		 * 事實上為 http 指令，Servlet 將網址丟給瀏覽器，
+		 * 要求瀏覽器執行這個網頁路徑(JSP)，並不是 Service 端重新執行。
+		 * 可以寫成：
+		 * return "redirect:http://localhost:8080/ssr/book";
+		 * 意思就是 addBook 回傳 redirect ssr/book，
+		 * 要求瀏覽器重新執行 public String findAllBooks(Model model)，顯示所有書籍。
+		 */
+	}
+	
+	// 刪除書籍
+	@DeleteMapping("/ssr/book/delete/{id}")
+	public String deleteBook(@PathVariable Integer id, Model model) {
+		try {
+			bookService.deleteBook(id);
+		} catch (BookException e) {
+			model.addAttribute("message", "刪除時發生錯誤" + e.getMessage());
+			return "error";
+		}
+		return "redirect:/ssr/book"; 
 	}
 }
--- Spring/springboot-mvc/src/main/webapp/WEB-INF/view/include/menu.jsp
+++ Spring/springboot-mvc/src/main/webapp/WEB-INF/view/include/menu.jsp
@@ -1,8 +1,8 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <%@ taglib prefix="fmt" uri="jakarta.tags.fmt" %>
-<%@ taglib prefix="fn" uri="jakarta.tags.functions" %>
-<%@ page import="java.util.List"%>
+<%@ taglib prefix="fn" uri="jakarta.tags.functions" %>    
 <div>
-	<a = href="/ssr/book">📔 書籍列表</a> | <a href = "/ssr/book/add">➕ 新增書籍</a>
+	<a href="/ssr/book">📚 書籍列表</a> 
 </div>
