ef8b39d053c4bb073ba8f2964efaaca9a12e7353
KrisYangPO <kristoff0109@gmail.com>
2025-05-20 13:25:06 +0800

    JPA + react deployment

--- Spring/springboot-jpa/src/main/java/com/example/demo/response/ApiResponse.java
+++ Spring/springboot-jpa/src/main/java/com/example/demo/response/ApiResponse.java
@@ -9,16 +9,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
+	private Integer status; 
 	private String message; // 訊息 例如: 查詢成功, 新增成功, 計算成功...
 	T data;                 // payload 實際資料
 	
 	// 成功回應
 	public static <T> ApiResponse<T> success(String message, T data) {
-		return new ApiResponse<T>(message, data);
+		return new ApiResponse<T>(200, message, data);
 	}
 	
 	// 失敗回應
-	public static <T> ApiResponse<T> error(String message) {
-		return new ApiResponse<T>(message, null);
+	public static <T> ApiResponse<T> error(Integer status, String message) {
+		return new ApiResponse<T>(status, message, null);
 	}
 }
