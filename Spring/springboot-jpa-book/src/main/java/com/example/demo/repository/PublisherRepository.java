cbd877975f00ccb5762acd5ae3e33cfe0cd5e184
KrisYangPO <kristoff0109@gmail.com>
2025-05-26 18:11:59 +0800

    update

--- Spring/springboot-jpa-book/src/main/java/com/example/demo/repository/PublisherRepository.java
+++ Spring/springboot-jpa-book/src/main/java/com/example/demo/repository/PublisherRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.entity.Author;
 import com.example.demo.model.entity.Publisher;
 
 @Repository
